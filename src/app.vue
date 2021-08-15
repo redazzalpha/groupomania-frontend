3bc0807e461bb439e74170f7f9ba9399e22d00d6
kyzer <redazzkillah@gmail.com>
2021-08-15 15:43:55 +0200

    remove unless refresh

--- src/app.vue
+++ src/app.vue
@@ -11,73 +11,10 @@
 </template>
 
 <script>
-import {mapState, mapActions} from 'vuex';
-import defines from "./defines/define";
 import hdr from "./components/header.vue";
 export default {
     components: {
         hdr,
-    },
-    computed: {
-        ...mapState(["userData", "publications", "notifs",]),
-    },
-    methods: {
-        ...mapActions(["setUserData", "setPublications", "setNotifs"]),
-        getPubs() {
-            return new Promise((resolve, reject) => {
-                // get request
-                this.$http.get(`${defines.SERVER_URL}${defines.PUBLISH_URL}`)
-                .then(
-                    (success) => {
-                        this.setPublications(success.body.results);
-                        resolve();
-                    },
-                    (failed) => {
-                        reject(failed);
-                    }
-                );
-            });
-        },
-        getComs() {
-            return new Promise((resolve, reject) => {
-                // get request
-                this.$http.get(`${defines.SERVER_URL}${defines.COMMENT_URL}`)
-                .then(
-                    (success) => {
-                        this.comments = success.body.results;
-                        resolve();
-                    },
-                    (failed) => {reject(failed);}
-                );
-            });
-        },
-        getNotifs() {
-            return new Promise((resolve, reject) => {
-                this.$http.get(`${defines.SERVER_URL}${defines.NOTIFICATION_URL}`)
-                .then(
-                    (success) => {
-                        const notifs = success.body.results.filter(item => item.whereId == this.userData.userId);
-                        this.setNotifs(notifs);
-                        resolve();
-                    },
-                    (failed) => { reject(failed); }
-                );
-            });
-        },
-        refresh() {
-            this.getPubs()
-                .then( () => {
-                    this.getComs()
-                        .then( () => {
-                            this.getNotifs()
-                        })
-                        .catch();
-                })
-                .catch();
-        },
-    },
-    mounted() {
-        this.refresh();
     },
 }
 </script>
--- src/views/notification.vue
+++ src/views/notification.vue
@@ -147,7 +147,7 @@ export default {
         },
     },
     mounted() {
-        this.refresh();
+        //this.refresh();
     },
 }
 </script>
